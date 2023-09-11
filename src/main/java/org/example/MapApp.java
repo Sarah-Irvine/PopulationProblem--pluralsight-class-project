d1c1593810779d6ed12d665a67e37d5c43e4cb5f
zs110313 <zedsheikh110@gmail.com>
2023-09-11 10:38:24 +0100

    Add User Strings

--- src/main/java/org/example/MapApp.java
+++ src/main/java/org/example/MapApp.java
@@ -1,18 +1,21 @@
 package org.example;
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class MapApp {
     public static void main(String[] args) {
-        CountryMap country = new CountryMap();
 
-        for (Country c : country.countryPopulations){
-            System.out.println(c.getName());
-        }
+        Scanner myScanner = new Scanner(System.in);
 
-        System.out.println(country.countryPopulations.first());
-        System.out.println(country.countryPopulations.last());
+        System.out.println("Press A to show the country with the largest population");
+        System.out.println("Press B to show the country with the smallest population");
+        System.out.println("Press C to show a list of countries in order from lowest population to highest population");
+        System.out.println("Press D to add a country");
+        System.out.println("Press E to remove a country");
+        System.out.println("Press F to exit");
+
+        String choice = myScanner.nextLine();
+
     }
-
-
 }
