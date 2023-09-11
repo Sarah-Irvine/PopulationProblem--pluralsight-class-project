9456bedc4521c11c8a2a9a0a5d99c25a3746a8bc
Master-Bobba <bayvazov94@gmail.com>
2023-09-11 09:44:28 +0100

    country class and comparator

--- src/main/java/org/example/CountryMap.java
+++ src/main/java/org/example/CountryMap.java
@@ -1,17 +1,29 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class CountryMap {
-    private HashMap<String, Integer> countryPopulations = new HashMap<>();
+    private HashMap<String, Country> countries = new HashMap<>();
+    public TreeSet<Country> countryPopulations = new TreeSet<>(countries.values());
 
-    public void CountryMap (){
+    public CountryMap (){
         populate();
     }
 
     private void populate(){
-        countryPopulations.put("UK",500);
-        countryPopulations.put("US",5000);
 
+        countries.put("uk", new Country("uk", 500) );
+        countries.put("US", new Country("US", 5000));
+        countries.put("GER", new Country("GER", 670));
+        countries.put("GB", new Country("GB", 5034));
+
+        System.out.println(countries.values());
+        System.out.println(countryPopulations.size());
     }
 }
+
+/**
+ * https://beginnersbook.com/2014/07/how-to-sort-a-treemap-by-value-in-java/
+ */
--- src/main/java/org/example/MapApp.java
+++ src/main/java/org/example/MapApp.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import java.util.Iterator;
+
 public class MapApp {
     public static void main(String[] args) {
         CountryMap country = new CountryMap();
+
+        for (Country c : country.countryPopulations){
+            System.out.println(c.getName());
+        }
+
+        System.out.println(country.countryPopulations.first());
+        System.out.println(country.countryPopulations.last());
     }
 
 
